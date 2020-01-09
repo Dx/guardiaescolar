c3b0332f3fbae35caa95781163be1ceb4012f00d
Dx <dequis@gmail.com>
2020-01-09 08:39:25 -0600

    Initial Commit

---
+++ MiGuardianEscolar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MiGuardianEscolar
+//
+//  Created by Dx on 09/01/20.
+//  Copyright © 2020 heimtek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
